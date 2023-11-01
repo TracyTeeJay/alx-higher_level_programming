be83c9c660e4621d718754145916ecb7cdfd923b
TracyTeeJay <tracyjepkoech@yahoo.com>
2023-11-01 16:33:03 -0700

    task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print(f"{chr(letter)}", end="")
+print("".join("{}".format(chr(letter)) for letter in range
+              (ord('a'), ord('z') + 1)), end="")
