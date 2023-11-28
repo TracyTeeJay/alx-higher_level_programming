2be10d30b11df7d16860c16bdd7e94bc992d37f9
TracyTeeJay <tracyjepkoech@yahoo.com>
2023-11-28 02:09:10 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+
+this module has a calss named: Square
+
+
+"""
+
+
+class Square:
+    """
+
+    this is class Square has the following:
+
+    Attributes:
+    __size(int): an attribute size containing the length
+
+    """
+
+    def __init__(self, size):
+        """
+        this is called when size is initialized
+        Args:
+            size (int): this is used to initialize the __size private attribute
+        """
+        self.__size = size
