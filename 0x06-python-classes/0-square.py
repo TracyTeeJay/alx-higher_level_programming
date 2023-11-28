787cb66cd84ef420fe7ca007db35b60043d10a74
TracyTeeJay <tracyjepkoech@yahoo.com>
2023-11-28 01:03:40 -0800

    task0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+
+this module contains a calss with the name: square
+
+"""
+
+class Square:
+    """
+
+    this is an empty class with no methods or attributes
+
+    """
+
+pass
