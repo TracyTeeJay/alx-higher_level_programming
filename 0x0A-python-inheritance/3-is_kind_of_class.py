8b1e070dc0933de69d787b550134a02c8ed5c7b4
TracyTeeJay <tracyjepkoech@yahoo.com>
2023-12-05 05:32:57 -0800

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+contains class is_kind_of_class(obj, a_class):
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns true or false if object is an instance of class
+    """
+    return (isinstance(obj, a_class))
