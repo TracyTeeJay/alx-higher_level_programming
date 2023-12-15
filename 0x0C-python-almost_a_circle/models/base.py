b809f803e4aa00423dd618c28b86fc2c8d7ddc00
TracyTeeJay <tracyjepkoech@yahoo.com>
2023-12-15 02:41:44 -0800

    task15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 contains class called Base
 """
 
+import json
+
 
 class Base:
     """
@@ -25,3 +27,13 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns a json string
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
