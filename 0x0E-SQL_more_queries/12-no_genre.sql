14dcf3403c43f0560e9ac00bf97cc13af8e0e04b
TracyTeeJay <tracyjepkoech@yahoo.com>
2024-02-08 07:17:50 -0800

    tasks5-16

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- script to import from databse and enlist tv-shows without genre link
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
