65a6426313fbde3d059c68947121f48f91a48508
TracyTeeJay <tracyjepkoech@yahoo.com>
2024-04-09 16:34:30 -0700

    tasks 0-5

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let cnt1 = 0; cnt1 < this.height; cnt1++) {
+      let plcHld = '';
+      for (let cnt2 = 0; cnt2 < this.width; cnt2++) {
+        plcHld += 'X';
+      }
+      console.log(plcHld);
+    }
+  }
+
+  rotate () {
+    const tempr = this.width;
+    this.width = this.height;
+    this.height = tempr;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
